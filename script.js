346ca03aeeac5917241fee157eec4960e91803a7
jgporter24 <110738676+jgporter24@users.noreply.github.com>
2022-09-23 16:48:23 -0500

    content updated index and css started on script

--- script.js
+++ script.js
@@ -1,56 +1,17 @@
-
-
-
-// var timerEl = document.getElementById('countdown');
-// var mainEl = document.getElementById('main');
-
-// var message =
-//   'Some say the world will end in 🔥, Some say in ice. From what I’ve tasted of desire, I hold with those who favor fire. But if it had to perish twice, I think I know enough of hate. To say that for destruction ice, Is also great, And would suffice.';
-// var words = message.split(' ');
-
-// // Timer that counts down from 5
-// function countdown() {
-//   var timeLeft = 5;
-
-//   // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
-//   var timeInterval = setInterval(function () {
-//     // As long as the `timeLeft` is greater than 1
-//     if (timeLeft > 1) {
-//       // Set the `textContent` of `timerEl` to show the remaining seconds
-//       timerEl.textContent = timeLeft + ' seconds remaining';
-//       // Decrement `timeLeft` by 1
-//       timeLeft--;
-//     } else if (timeLeft === 1) {
-//       // When `timeLeft` is equal to 1, rename to 'second' instead of 'seconds'
-//       timerEl.textContent = timeLeft + ' second remaining';
-//       timeLeft--;
-//     } else {
-//       // Once `timeLeft` gets to 0, set `timerEl` to an empty string
-//       timerEl.textContent = '';
-//       // Use `clearInterval()` to stop the timer
-//       clearInterval(timeInterval);
-//       // Call the `displayMessage()` function
-//       displayMessage();
-//     }
-//   }, 1000);
-// }
-
-// // Displays the message one word at a time
-// function displayMessage() {
-//   var wordCount = 0;
-
-//   // Uses the `setInterval()` method to call a function to be executed every 1000 milliseconds
-//   var msgInterval = setInterval(function () {
-//     // If there are no more words left in the message
-//     if (words[wordCount] === undefined) {
-//       // Use `clearInterval()` to stop the timer
-//       clearInterval(msgInterval);
-//     } else {
-//       // Display one word of the message
-//       mainEl.textContent = words[wordCount];
-//       wordCount++;
-//     }
-//   }, 1000);
-// }
-
-// countdown();
+//Define variables
+    var welcome = document.querySelector("#introduction");
+    var startBtn = document.querySelector("#button_start");
+    var introPage =document.querySelector("#intro");
+    
+    var questionPage = document.querySelector("#question_page");
+    var askQuestion = document.querySelector("#ask_question");
+    
+    var reactButtons = document.querySelectorAll(".choices");
+    var answerBtn1 = document.querySelector("#answer1");
+    var answerBtn2 = document.querySelector("#answer2");
+    var answerBtn3 = document.querySelector("#answer3");
+    var answerBtn4 = document.querySelector("#answer4");
+    
+    var checkRslt = document.querySelector("#check_result");
+    var gradeBoard = document.querySelector("#outcome_page");
+    
