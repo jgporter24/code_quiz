f926bf8879324c42e2d43fec13ba86e58d8b3dd0
jgporter24 <110738676+jgporter24@users.noreply.github.com>
2022-09-24 07:45:44 -0500

    added questions to js

--- script.js
+++ script.js
@@ -14,4 +14,59 @@
     
     var checkRslt = document.querySelector("#check_result");
     var gradeBoard = document.querySelector("#outcome_page");
+    var finalScore = document.querySelector("#final_score");
+    var userInitial =document.querySelector("#initial");
     
+    var submitBtn =document.querySelector("#submit_btn");
+    var highScorePage =document.querySelector("#highscore_page");
+    var scoreRecord =document.querySelector("#score_record");
+    var scoreCheck =document.querySelector("#check_score");
+    var finish =document.querySelector("#finish");
+    
+    var backBtn =document.querySelector("#back_btn");
+    var clearBtn=document.querySelector("#clear_btn");
+    
+        //Define questions (Object)
+    var questionSource = [
+        {
+            question: "Questions 1 : Inside which HTML element do we put the JavaScript?",
+            choices: ["a. <javascript>", "b. <js>", "c. <script>", "d. <scripting>"],
+            answer: "c"
+        },
+        {
+            question: "Questions 2 : How to write an IF statement in JavaScript?",
+            choices: ["a. if i == 5 then", "b. if i === 5 then", "c. if i = 5", "d. if(i == 5)"],
+            answer: "d"
+        },
+        {
+            question: "Questions 3 : How do you create a function in JavaScript?",
+            choices: ["a. function = myFunction()", "b. function myFunction()", "c. function:myFunction()", "d. createMyFunction()"],
+            answer: "b"
+        },
+        {
+            question: "Questions 4 : How do you call a function named myFunction?",
+            choices: ["a. call myFunction()", "b. call function myFunction()", "c. myFunction()", "d. call myFunction"],
+            answer: "c"
+        },
+        {
+            question: "Questions 5 : How do you write 'Hello World' in an alert box?",
+            choices: ["a. msgBox('Hello World')", "b. alert('Hellow World')", "c. alertBox('Hellow World')", "d. msg('Hellow World')"],
+            answer: "b"
+        },
+        {
+            question: "Questions 6 : The first index of an array is ____.",
+            choices: ["a. 2", "b. 4", "c. 0", "d. None of the above"],
+            answer: "c"
+        },
+        {
+            question: "Questions 7 : Commonly used data types DO NOT include:",
+            choices: ["a. strings", "b. booleans", "c. alerts", "d. numbers"],
+            answer: "c"
+        },
+        {
+            question: "Questions 8 : Which event occurs when the user clicks on an HTML element?",
+            choices: ["a. onclick", "b. onchange", "c. onmouseover", "d. onmouseclick"],
+            answer: "a"
+        }
+    ];
+       
